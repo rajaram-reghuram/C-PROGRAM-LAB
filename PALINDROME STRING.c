8d0f530486c23354d46ab3438c6543c7eaebc0e9
RAJARAM REGHURAM <61812107+rajaram-reghuram@users.noreply.github.com>
2020-05-25 15:05:53 +0530

    PALINDROME STRING.c

--- PALINDROME STRING.c
+++ PALINDROME STRING.c
@@ -11,17 +11,20 @@ int main(){
     
     length = strlen(string1);
     
-    for(i=0;i < length ;i++){
+    for(i=0;i < length ;i++)
+    {
         if(string1[i] != string1[length-i-1]){
             flag = 1;
             break;
    }
-}
+
     
-    if (flag) {
+    if (flag)
+    {
         printf("%s is not a palindrome", string1);
     }    
-    else {
+    else
+    {
         printf("%s is a palindrome", string1);
     }
     return 0;
