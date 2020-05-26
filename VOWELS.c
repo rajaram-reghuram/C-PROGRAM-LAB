610038247476f298e8a96b492cda59e80ce4003c
RAJARAM REGHURAM <61812107+rajaram-reghuram@users.noreply.github.com>
2020-05-26 19:28:49 +0530

    VOWELS.c

---
+++ VOWELS.c
@@ -0,0 +1,29 @@
+
+#include<stdio.h>
+
+#include<string.h>
+
+void main()
+	{
+		int vow_count=0, const_count=0, sp_count=0;
+		char str[50];
+
+		printf("\n Enter a string: ");
+		gets(str);
+
+		for(int i=0; i<strlen(str); i++)
+			{
+				if(str[i] == ' ')
+					sp_count++;
+				
+				else if (str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' || str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U')
+					vow_count++;
+				
+				else if ((str[i] > 'a' && str[i] < 'z') || (str[i] > 'A' && str[i] < 'Z'))
+					const_count++;
+			}
+		
+		printf("\n Counts are as follows:- \n Vowels: %d \n Consonants: %d \n Whitespaces: %d \n", vow_count, const_count, sp_count);
+
+	}
+		
