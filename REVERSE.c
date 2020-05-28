9d15fb9a81f23c1006b05ffcfaf837f88e959897
RAJARAM REGHURAM <61812107+rajaram-reghuram@users.noreply.github.com>
2020-05-28 21:10:18 +0530

    REVERSE.c

--- REVERSE.c
+++ REVERSE.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+void reversestr(char str[])
+{ int i,n;
+  char c;
+  n=strlen(str);
+  for(i=0;i<n/2;i++)
+    {   c=str[i];
+         str[i]=str[n-1-i];
+         str[n-1-i]=c;
+     }
+}
+int main()
+{
+  char str[100];
 
-void reverse_str(char str[])
-	{ 
-		int i,n;
-  		char c;
-  		
-		n = strlen(str);
-  
-		for(i=0;i<n/2;i++)
-    			{   
-				c = str[i];
-         			str[i] = str[n-1-i];
-         			str[n-1-i] = c;
-     			}
-	}
-
-
-void main()
-	{
-  		char str[100];
- 
-  		printf("\n Enter the string: "); 
-  		scanf("%s",str);
-  
-		reverse_str(str);
-  		
-		printf("\n Reversed string is: %s \n",str);
+  printf("Enter the string \n"); 
+  scanf("%[^\n]",str);
+  reversestr(str);
+  printf("Reversed string is=%s\n",str);
 }
